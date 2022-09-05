468bbba6fed96a1f73a09c29ddbb5077d53bec47
Taewan Lim <taewan.seoul@gmail.com>
2022-09-05 22:45:06 +0900

    ✨ feat: creat WelcomeScreen for a tooltip image

--- App.tsx
+++ App.tsx
@@ -1,5 +1,32 @@
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
 import MainScreen from "./src/screens/MainScreen";
+import WelcomeScreen from "./src/screens/WelcomeScreen";
+
+type RootStackParamList = {
+  MainScreen: undefined;
+  WelcomeScreen: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
-  return <MainScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="MainScreen"
+          component={MainScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="WelcomeScreen"
+          component={WelcomeScreen}
+          options={{ headerShown: false, presentation: "transparentModal" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
