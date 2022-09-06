8db6f27648b4ee9fc5627c086d15dae7096230e6
Sujeong Park <krystarline@gmail.com>
2022-09-07 01:01:40 +0900

    ✨ feat: implement title input save method

--- src/components/Routine.tsx
+++ src/components/Routine.tsx
@@ -2,15 +2,18 @@ import { StyleSheet, View, Text, TextInput } from "react-native";
 
 export default function Routine() {
   return (
-    <View>
-      <Text style={styles.routineLabel}>Today Routine</Text>
-      <TextInput style={styles.routineInput} multiline />
+    <View style={styles.textInput}>
+      <Text style={styles.routinlabel}>Today Routine</Text>
+      <TextInput style={styles.routineInput} returnKeyType="done" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  routineLabel: {
+  textInput: {
+    width: "100%",
+  },
+  routinlabel: {
     fontSize: 15,
     fontWeight: "700",
     color: "#006de9",
--- src/components/TitleInput.tsx
+++ src/components/TitleInput.tsx
@@ -1,19 +1,60 @@
+import { SetStateAction, useState, useEffect } from "react";
 import { StyleSheet, View, Text, TextInput } from "react-native";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const STORAGE_TITLE_KEY = "@title";
+
 export default function TitleInput() {
+  const [title, setTitle] = useState<string>("");
+
+  const onChangeTitle = (payload: SetStateAction<string>) => {
+    setTitle(payload);
+  };
+
+  const onSaveTitle = async () => {
+    try {
+      await AsyncStorage.setItem(STORAGE_TITLE_KEY, title);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onLoadTitle = async () => {
+    try {
+      const value = await AsyncStorage.getItem(STORAGE_TITLE_KEY);
+
+      if (value !== null) {
+        setTitle(value as string);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    onLoadTitle();
+  }, []);
+
   return (
-    <View>
+    <View style={styles.textInput}>
       <Text style={styles.titleLabel}>Today's Title</Text>
       <TextInput
         placeholder="What is your focus for today?"
         style={styles.titleInput}
-        multiline
+        returnKeyType="done"
+        value={title}
+        onChangeText={onChangeTitle}
+        onSubmitEditing={onSaveTitle}
       />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  textInput: {
+    width: "100%",
+  },
   titleLabel: {
     fontSize: 15,
     fontWeight: "700",
