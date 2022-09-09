5f80a9760e30936c2c1db52fca612dd0c721c24d
Taewan Lim <taewan.seoul@gmail.com>
2022-09-09 15:19:15 +0900

    ✨ feat: create RoutineTemplate component

---
+++ src/components/RoutineTemplate.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+
+import { Entypo } from "@expo/vector-icons";
+
+export default function RoutineTemplate() {
+  const handleCardClick = () => {
+    console.log("carouselCard clicked!");
+  };
+
+  return (
+    <TouchableOpacity onPress={handleCardClick}>
+      <View style={styles.templateContainer}>
+        <Entypo
+          name="plus"
+          size={24}
+          color="#1a81f4"
+          style={{
+            paddingTop: 30,
+          }}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  templateContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+});
