90a8c714cf0e80454325571201e08da0cac1e607
Taewan Lim <taewan.seoul@gmail.com>
2022-09-16 22:36:44 +0900

    ✨ feat: create getDateFormat util to avoid duplication of newly created routine CardTitle

--- src/components/RoutineTemplate.tsx
+++ src/components/RoutineTemplate.tsx
@@ -4,6 +4,8 @@ import { Dimensions, StyleSheet, TouchableOpacity, View } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Entypo } from "@expo/vector-icons";
 
+import getDateFormat from "../utils/getDateFormat";
+
 export default function RoutineTemplate({
   cardTitleList,
   setRoutineTitleList,
@@ -12,19 +14,24 @@ export default function RoutineTemplate({
   setRoutineTitleList: Dispatch<SetStateAction<string[]>>;
 }) {
   const handleRoutineTemplateClick = async () => {
+    const dateMilliseconds = Date.now();
+
     const copyCardTitleList = cardTitleList.slice(0, -1);
-    const newCardTitleList = [...copyCardTitleList, "untitled routine"];
+    const newCardTitleList = [
+      ...copyCardTitleList,
+      getDateFormat(dateMilliseconds),
+    ];
     const initialTodoList = [
       {
-        id: `${Date.now()}`,
-        routineTitle: "untitled routine",
+        id: dateMilliseconds,
+        routineTitle: getDateFormat(dateMilliseconds),
         text: "",
         isChecked: false,
       },
     ];
 
     await AsyncStorage.setItem(
-      "@routine_untitled routine",
+      `@routine_${getDateFormat(dateMilliseconds)}`,
       JSON.stringify(initialTodoList),
     );
 
