579bf406a369e7fb270bac6ab169af53acb7c913
Taewan Lim <taewan.seoul@gmail.com>
2022-09-16 15:12:42 +0900

    ✨ feat: apply RoutineTemplate intead of TodoList on the last index of CarouselCard list

--- src/components/RoutineTemplate.tsx
+++ src/components/RoutineTemplate.tsx
@@ -1,23 +1,45 @@
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { Dispatch, SetStateAction } from "react";
+import { Dimensions, StyleSheet, TouchableOpacity, View } from "react-native";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Entypo } from "@expo/vector-icons";
 
-export default function RoutineTemplate() {
-  const handleCardClick = () => {
-    console.log("Would you like to create a new routine?");
+export default function RoutineTemplate({
+  cardTitleList,
+  setRoutineTitleList,
+}: {
+  cardTitleList: string[];
+  setRoutineTitleList: Dispatch<SetStateAction<string[]>>;
+}) {
+  const handleRoutineTemplateClick = async () => {
+    const copyCardTitleList = cardTitleList.slice(0, -1);
+    const newCardTitleList = [...copyCardTitleList, "untitled routine"];
+    const initialTodoList = [
+      {
+        id: `${Date.now()}`,
+        routineTitle: "untitled",
+        text: "",
+        isChecked: false,
+      },
+    ];
+
+    await AsyncStorage.setItem(
+      "@routine_untitled routine",
+      JSON.stringify(initialTodoList),
+    );
+
+    await AsyncStorage.setItem(
+      "@routineTitleList",
+      JSON.stringify(newCardTitleList),
+    );
+
+    setRoutineTitleList(newCardTitleList);
   };
 
   return (
-    <TouchableOpacity onPress={handleCardClick}>
+    <TouchableOpacity onPress={handleRoutineTemplateClick}>
       <View style={styles.templateContainer}>
-        <Entypo
-          name="plus"
-          size={24}
-          color="#1a81f4"
-          style={{
-            paddingTop: 30,
-          }}
-        />
+        <Entypo name="plus" size={24} color="#1a81f4" />
       </View>
     </TouchableOpacity>
   );
@@ -25,7 +47,8 @@ export default function RoutineTemplate() {
 
 const styles = StyleSheet.create({
   templateContainer: {
-    flex: 1,
+    height: Dimensions.get("screen").height / 2,
     alignItems: "center",
+    justifyContent: "center",
   },
 });
