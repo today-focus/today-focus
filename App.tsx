28ce09fa86ecb6237202b87e392eff446b30539e
Taewan Lim <taewan.seoul@gmail.com>
2022-09-25 15:03:24 +0900

    ✨ feat: set up onboarding screen logic using AsyncStoarage

--- App.tsx
+++ App.tsx
@@ -1,29 +1,67 @@
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { useEffect, useState } from "react";
 
-import MainScreen from "./src/screens/MainScreen";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import OnboardingScreen from "./src/screens/OnboardingScreen";
 import WelcomeScreen from "./src/screens/WelcomeScreen";
+import MainScreen from "./src/screens/MainScreen";
 
 import { RootStackParamList } from "./src/types";
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
+  const [isOnboarded, setIsOnboarded] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getInitialValue = async () => {
+      try {
+        const initialValue = await AsyncStorage.getItem("@isOnboarded");
+
+        if (initialValue !== null) {
+          setIsOnboarded("true");
+        } else {
+          setIsOnboarded("false");
+        }
+      } catch (error) {
+        console.log("Error @isOnboarded", error);
+      }
+    };
+
+    getInitialValue();
+  }, []);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="MainScreen"
-          component={MainScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="WelcomeScreen"
-          component={WelcomeScreen}
-          options={{ headerShown: false, presentation: "transparentModal" }}
-        />
-      </Stack.Navigator>
+      {isOnboarded !== null && (
+        <Stack.Navigator>
+          {isOnboarded === "true" && (
+            <>
+              <Stack.Screen
+                name="OnboardingScreen"
+                component={OnboardingScreen}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="WelcomeScreen"
+                component={WelcomeScreen}
+                options={{
+                  headerShown: false,
+                  presentation: "transparentModal",
+                }}
+              />
+            </>
+          )}
+          <Stack.Screen
+            name="MainScreen"
+            component={MainScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
