74e189d7e84a646d807ef436c355c8e44d51529d
Taewan Lim <taewan.seoul@gmail.com>
2022-10-10 14:56:15 +0900

    ➕ chore: set up jest-native config

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: [
+      ["@babel/preset-env", { targets: { node: "current" } }],
+      "@babel/preset-typescript",
+      "babel-preset-expo",
+    ],
     plugins: ["react-native-reanimated/plugin"],
   };
 };
